b05f71607d2b5390eade14335e470b9bcb43cbca
Jonathan <jleocan773@g.educaand.es>
2023-09-20 14:32:12 +0200

    Primera Actividad

---
+++ Tema-01/ejemplos/01/index.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+
+        Ejemplo 1 PHP 
+        Hola Mundo
+    
+    */
+
+    echo "<h1>";
+    echo "<center>";
+    echo "Hola Mundo PHP";
+    echo "<br/>";
+    echo "Soy Jonathan León Canto";
+    echo "<hr>";
+    echo "</center>";
+    echo "</h1>";
+    echo "Esta es la primera actividad de DWESE";
+
+
+?>
