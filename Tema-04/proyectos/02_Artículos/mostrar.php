0329495e89d043a3360c301f293f6a03b650de7b
Jonathan <jleocan773@g.educaand.es>
2023-11-13 14:22:39 +0100

    JLC | 11/11/2023 | Proyecto 4.2 Terminado

---
+++ Tema-04/proyectos/02_Artículos/mostrar.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+        Controlador: mostrar.php
+        Descripción: mostrar un formulario con los detalles no editables
+        del libro seleccionado
+    */
+
+
+// Clases
+require_once 'class/class.articulo.php';
+require_once 'class/class.arrayArticulos.php';
+
+// Librería
+include 'libs/crud_funciones.php';
+
+// Model
+include 'models/model.mostrar.php';
+
+
+// Vista
+include 'views/view.mostrar.php';
--- Tema-04/proyectos/02_Artículos/views/view.mostrar.php
+++ Tema-04/proyectos/02_Artículos/views/view.mostrar.php
@@ -3,7 +3,6 @@
 
 <head>
     <?php include 'views/layouts/head.html' ?>
-    <title>Proyecto 3.1 - Gestión de libros</title>
 </head>
 
 <body>
@@ -21,35 +20,35 @@
             <!-- id -->
             <div class="mb-3">
                 <label for="titulo" class="form-label">Id</label>
-                <input type="text" class="form-control" name="id" value="<?= $articulo['id'] ?>" disabled>
+                <input type="text" class="form-control" value="<?= $articulo-> getId() ?>" disabled>
             </div>
             <!-- Descripción -->
             <div class="mb-3">
                 <label for="descripcion" class="form-label">Descripción</label>
-                <input type="text" class="form-control" name="descripcion" value="<?= $articulo['descripcion'] ?>"
+                <input type="text" class="form-control" value="<?= $articulo-> getDescripcion() ?>"
                     disabled>
             </div>
             <!-- Modelo -->
             <div class="mb-3">
                 <label for="modelo" class="form-label">Modelo</label>
-                <input type="text" class="form-control" name="modelo" value="<?= $articulo['modelo'] ?>" disabled>
+                <input type="text" class="form-control" value="<?= $articulo-> getModelo() ?>" disabled>
             </div>
             <!-- Marca Select -->
             <div class="mb-3">
                 <label for="marca" class="form-label">Marca</label>
-                <input type="text" class="form-control" name="categoria"
-                    value="<?= $marcas[$articulo['marca']] ?>" disabled>
+                <input type="text" class="form-control"
+                    value="<?= $marcas[$articulo->getMarca()] ?>" disabled>
             </div>
             <!-- Unidades -->
             <div class="mb-3">
                 <label for="unidades" class="form-label">Unidades</label>
-                <input type="number" class="form-control" name="unidades" step="0.01"
-                    value="<?= $articulo['unidades'] ?>" disabled>
+                <input type="number" class="form-control" step="0.01"
+                    value="<?= $articulo->getUnidades() ?>" disabled>
             </div>
             <!-- Precio -->
             <div class="mb-3">
                 <label for="precio" class="form-label">Precio (€)</label>
-                <input type="number" class="form-control" name="precio" step="0.01" value="<?= $articulo['precio'] ?>"
+                <input type="number" class="form-control" step="0.01" value="<?= $articulo->getPrecio() ?>"
                     disabled>
             </div>
 
@@ -57,7 +56,7 @@
              <div class="mb-3">
                 <label for="marca" class="form-label">Categorías</label>
                 <input type="text" class="form-control" name="categorias"
-                    value="<?= implode(', ', mostrarCategorias($categorias, $articulo['categorias'])) ?>" disabled>
+                    value="<?= implode(', ', ArrayArticulos::mostrarCategorias($categorias, $articulo->getCategorias())) ?>" disabled>
             </div>
 
 
