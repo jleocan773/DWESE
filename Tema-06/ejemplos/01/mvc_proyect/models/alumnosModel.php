783b276f9327a374aaea7503d84df5f9e86c9aac
Jonathan <jleocan773@g.educaand.es>
2023-12-12 10:36:53 +0100

    JLC | 2023-12-12 | Proyecto MVC Ejemplo

---
+++ Tema-06/ejemplos/01/mvc_proyect/models/alumnosModel.php
@@ -0,0 +1,30 @@
+<?php
+
+    /*
+        alumnosModel.php
+
+        Modelo del controlador alumnos
+
+        Definir los métodos de acceso a la base de datos
+
+        - insert
+        - update
+        - select
+        - delete
+        - etc
+    
+        */
+
+    Class alumnosModel extends Model {
+
+        /*
+            Extrae los detalles de los alumnos
+        */
+
+        public function get(){
+            
+        }
+
+    }
+
+?>
---
+++ Tema-06/ejemplos/01/mvc_proyect/views/alumnos/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumnos</title>
+</head>
+<body>
+    BUENAS TARDES BUENAS TARDES BUENAS TARDES BUENAS TARDES
+</body>
+</html>
