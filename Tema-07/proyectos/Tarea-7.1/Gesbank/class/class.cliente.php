9129755f59d9a6ce24e2e8e18ee5c96ac3191322
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2024-01-20 00:18:54 +0100

    JLC | 2024-01-20 | Avance Proyecto 7.1 Validación -> Validación Clientes completa

--- Tema-07/proyectos/Tarea-7.1/Gesbank/class/class.cliente.php
+++ Tema-07/proyectos/Tarea-7.1/Gesbank/class/class.cliente.php
@@ -8,14 +8,14 @@ class classCliente
 
     public $id;
     public $apellidos;
-    public $nombre;       
+    public $nombre;
     public $telefono;
     public $ciudad;
     public $dni;
     public $email;
     public $create_at;
     public $update_at;
-    
+
 
     function __construct(
 
@@ -32,17 +32,14 @@ class classCliente
 
     ) {
 
-        $this->id=$id;
-        $this->nombre=$nombre;
-        $this->apellidos= $apellidos;
-        $this->email=$email;
-        $this->telefono= $telefono;
-        $this->dni= $dni;
-        $this->ciudad= $ciudad;
-        $this->create_at= $create_at;
-        $this->update_at= $update_at;
-
-        $this->sql = "select * from alumnos";
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->email = $email;
+        $this->telefono = $telefono;
+        $this->dni = $dni;
+        $this->ciudad = $ciudad;
+        $this->create_at = $create_at;
+        $this->update_at = $update_at;
     }
-
 }
