7e66d87867cd890ba706599106db376b8cb05535
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2024-01-22 19:06:47 +0100

    JLC | 2024-01-22 | Avance Proyecto 7.2 Autenticación Completada

---
+++ Tema-07/proyectos/Tarea-7.2/Gesbank/config/config.php
@@ -0,0 +1,12 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+define('URL', 'http://localhost/DWESE/Tema-07/proyectos/Tarea-7.2/Gesbank/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
---
+++ Tema-07/proyectos/Tarea-7.2/Gesbank/views/clientes/main/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- head -->
+    <?php require_once("template/partials/head.php");  ?>
+    <title>Clientes - Gesbank</title>
+</head>
+
+<body>
+    <div class="container" style="padding-top: 2%;">
+        <!-- menu fijo superior -->
+        <?php require_once "template/partials/menuAut.php"; ?>
+        <br><br>
+        <!-- cabecera  -->
+        <?php include "views/clientes/partials/header.php" ?>
+        <!-- Menu principal -->
+        <?php require_once "views/clientes/partials/menu.php" ?>
+        <!-- tabla clientes -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id </th>
+                    <th>Cliente</th>
+                    <th>Email</th>
+                    <th>Telefono</th>
+                    <th>Ciudad</th>
+                    <th>DNI</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($this->clientes as $cliente) : ?>
+                    <tr>
+                        <td><?= $cliente->id ?></td>
+                        <td><?= $cliente->cliente ?></td>
+                        <td><?= $cliente->email ?></td>
+                        <td><?= $cliente->telefono ?></td>
+                        <td><?= $cliente->ciudad ?></td>
+                        <td><?= $cliente->dni ?></td>
+                        <td>
+                            <!-- botones de acción -->
+                            <a href="<?= URL ?>clientes/delete/<?= $cliente->id ?>" title="Eliminar" onclick="return confirm('¿Quieres Borrar?')"> <i class="bi bi-trash"></i> </a>
+                            <a href="<?= URL ?>clientes/editar/<?= $cliente->id ?>" title="Editar"> <i class="bi bi-pencil"></i> </a>
+                            <a href="<?= URL ?>clientes/mostrar/<?= $cliente->id ?>" title="Mostrar"> <i class="bi bi-eye"></i> </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="8">Nº Registros: <?= $this->clientes->rowCount() ?> </td>
+                </tr>
+            </tfoot>
+
+        </table>
+
+    </div>
+
+    <!-- footer -->
+    <?php require_once "template/partials/footer.php" ?>
+
+    <!-- Bootstrap JS y popper -->
+    <?php require_once "template/partials/javascript.php" ?>
+</body>
+
+</html>
