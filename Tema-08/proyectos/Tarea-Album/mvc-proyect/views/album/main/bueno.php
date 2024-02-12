f9212a87d417c3429c9d782e8f06c7c73d7bcb21
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2024-02-12 17:44:40 +0100

    JLC | 2024-02-12 | Proyecto Álbum - Modal funciona perfecto

--- Tema-08/proyectos/Tarea-Album/mvc-proyect/views/album/main/bueno.php
+++ Tema-08/proyectos/Tarea-Album/mvc-proyect/views/album/main/bueno.php
@@ -8,6 +8,7 @@
 	<title>
 		<?= $this->title ?>
 	</title>
+
 </head>
 
 
@@ -35,7 +36,7 @@
 				Tabla de Albumes
 			</div>
 			<div class="card-header">
-				<!-- menu albumes -->
+				<!-- menu alumnos -->
 				<?php require_once("views/album/partials/menu.php") ?>
 			</div>
 			<div class="card-body">
@@ -48,12 +49,13 @@
 							<!-- personalizado -->
 							<th>Id</th>
 							<th>Titulo</th>
-							<th>Carpeta</th>
 							<th>Descripción</th>
 							<th>Autor</th>
+							<th>Lugar</th>
 							<th>Fecha</th>
 							<th>Categoría</th>
 							<th>Etiquetas</th>
+							<th>Carpetas</th>
 							<th>Acciones</th>
 						</tr>
 					</thead>
@@ -64,48 +66,63 @@
 						<?php foreach ($this->albumes as $album) : ?>
 							<?php require('template/partials/modal.php'); ?>
 							<tr>
-								<!-- Formatos distintos para cada  columna -->
-
-								<!-- Detalles de albums -->
-								<td><?= $album->id ?></td>
-								<td><?= $album->titulo ?></td>
-								<td><?= $album->carpeta ?></td>
-								<td><?= $album->descripcion ?></td>
-								<td><?= $album->autor ?></td>
-								<td><?= $album->fecha ?></td>
-								<td><?= $album->categoria ?></td>
-								<td><?= $album->etiquetas ?></td>
+								<!-- Detalles de albumes -->
+								<td>
+									<?= $album->id ?>
+								</td>
+								<td>
+									<?= $album->titulo ?>
+								</td>
+								<td>
+									<?= $album->descripcion ?>
+								</td>
+								<td>
+									<?= $album->autor ?>
+								</td>
+								<td>
+									<?= $album->lugar ?>
+								</td>
+								<td>
+									<?= $album->fecha ?>
+								</td>
+								<td>
+									<?= $album->categoria ?>
+								</td>
+								<td>
+									<?= $album->etiquetas ?>
+								</td>
+								<td>
+									<?= $album->carpeta ?>
+								</td>
 
 
 								<!-- botones de acción -->
 								<td>
-
-									<!-- botón eliminar -->
+									<!-- botón  eliminar -->
 									<a href="<?= URL ?>album/delete/<?= $album->id ?>" title="Eliminar" onclick="return confirm('Confimar elimación del album')" Class="btn btn-danger
 											<?= (!in_array($_SESSION['id_rol'], $GLOBALS['album']['delete'])) ?
 												'disabled' : null ?>">
 										<i class="bi bi-trash"></i>
 									</a>
 
-									<!-- botón editar -->
+									<!-- botón  editar -->
 									<a href="<?= URL ?>album/edit/<?= $album->id ?>" title="Editar" class="btn btn-primary
 											<?= (!in_array($_SESSION['id_rol'], $GLOBALS['album']['edit'])) ?
 												'disabled' : null ?>">
 										<i class="bi bi-pencil"></i>
 									</a>
 
-									<!-- botón mostrar -->
-									<a href="<?= URL ?>album/show/<?= $album->id ?>" title="Mostrar" class="btn btn-warning
+									<!-- botón  mostrar -->
+									<a href="<?= URL ?>album/show/<?= $album->id ?> ?>" title="Mostrar" class="btn btn-warning
 											<?= (!in_array($_SESSION['id_rol'], $GLOBALS['album']['show'])) ?
 												'disabled' : null ?>">
 										<i class="bi bi-card-text"></i>
 									</a>
-
-									<!-- botón modal -->
-									<a href="#" title="Subir" data-bs-toggle="modal" data-bs-target="#subir<?= $album->id ?>" class="btn btn-success <?= (!in_array($_SESSION['id_rol'], $GLOBALS['album']['add'])) ? 'disabled' : null ?> ">
-										<i class="bi bi-cloud-upload-fill"></i>
+									<!-- botón  subir imagen -->
+									<a href="#" title="Subir" data-bs-toggle="modal" data-bs-target="#subir<?= $album->id ?>" class="btn btn-success <?= (!in_array($_SESSION['id_rol'], $GLOBALS['album']['add'])) ?
+																																							'disabled' : null ?> ">
+										<i class="bi bi-image"></i>
 									</a>
-
 								</td>
 							</tr>
 
@@ -118,7 +135,7 @@
 
 			</div>
 			<div class="card-footer">
-				<small class="text-muted"> Albumes existentes:
+				<small class="text-muted"> Albumes:
 					<?= $this->albumes->rowCount(); ?>
 				</small>
 			</div>
